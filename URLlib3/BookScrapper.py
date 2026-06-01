2b4421a4edb99c8decc4b8512cda5dacbb0b150b
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2026-06-01 15:31:55 +0530

    Book Scrapper-URLLIB

---
+++ URLlib3/BookScrapper.py
@@ -0,0 +1,20 @@
+import urllib3
+import json
+import pandas as pd
+
+# Input Variables
+url = "https://www.goodreads.com/choiceawards/best-books-2025"
+
+    
+#Parising URL with UrlLib
+reponse = urllib3.request("GET",url)
+
+if reponse.status == 200:
+    json_text = reponse.data.decode("utf-8")
+    print(json_text)
+    with open("Book-Review.HTML","w",encoding='utf-8')as file:
+        json.dump(json_text,file, indent=4, ensure_ascii= False)
+        print("File Saved")
+else:
+    print(f"Failed to fetch data. HTTP Status: {reponse.status}")
+
